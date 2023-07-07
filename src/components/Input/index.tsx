53e54dc9764fa6b4c0cda96a4ce632e09fad82df
Jonathan Candido <jonathancsrr@gmail.com>
2023-07-07 19:37:43 -0300

    chore: Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from "react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+export function Input({ ...props }: InputProps) {
+  return <input {...props} type="text" />;
+}
